7a1cf8bc75e6410b86a494c704b66079f99141bf
Hugo <gdhugo666@gmail.com>
2024-08-01 10:13:25 +0200

    suerte en frases

---
+++ js/array.js
@@ -0,0 +1,35 @@
+//arrays
+function suerte(){
+    let frases=[
+        "se te va a romper movil",
+        "Nunca pienso en las consecuencias de fallar un gran tiro… cuando se piensa en las consecuencias se está pensando en un resultado negativo",
+        "El dinero no es la clave del éxito; la libertad para poder crear lo es",
+        "Cuanto más duramente trabajo, más suerte tengo",
+        "La inteligencia consiste no solo en el conocimiento, sino también en la destreza de aplicar los conocimientos en la práctica",
+        "El trabajo duro hace que desaparezcan las arrugas de la mente y el espíritu ",
+        "Cuando algo es lo suficientemente importante, lo haces incluso si las probabilidades de que salga bien no te acompañan",
+        "Cuéntamelo y me olvidaré. Enséñamelo y lo recordaré. Involúcrame y lo aprenderé ",
+    ]
+    
+    
+    // console.log(frutas[1]);
+    // frutas.push("fresas");
+    // console.table(frutas);
+    // console.log(frutas.length);
+    
+    // for (let i=0; i<frutas.length;i++){
+    //     console.log(frutas[i]);
+    // }
+    
+    // frutas.forEach(fruta=>{
+    //     console.log(fruta);
+    // });
+    
+    // frutas.forEach((fruta,indice,array)=>{
+    //     console.log(fruta+" "+indice);
+    // });
+    let ale=Math.floor(Math.random()*frases.length);
+
+    document.getElementById("resultado").textContent=frases[ale];
+}
+
