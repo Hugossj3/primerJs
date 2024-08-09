ac733fed8e1efa94c015729472ca36772a4b3bc8
Hugo <gdhugo666@gmail.com>
2024-08-09 10:20:00 +0200

    buenas no quisiera molestar

---
+++ js/saludo.js
@@ -0,0 +1,40 @@
+const saludoTxt = document.getElementById('saludo');
+const nameInputContainer = document.getElementById('name-input-container');
+const nameInput = document.getElementById('name-input');
+const submitButton = document.getElementById('submit-name');
+
+function guardarNombre(){
+    const name = nameInput.value.trim();
+    if(name.length > 1){
+        localStorage.setItem('nombreUsuario', name);
+        mostrarNombre(name);
+    }
+}
+
+function mostrarNombre(name){
+    let h=new Date().getHours();
+    let tDia="";
+    if(h<12){
+        tDia="dias";
+    }else if(h<7){
+        tDia="tardes";
+    }else{
+        tDia="noches";
+    }
+    saludoTxt.textContent = `Buenas ${tDia}, ${name}`;
+    nameInputContainer.style.display = 'none';
+}
+
+submitButton.addEventListener('click', guardarNombre);
+nameInput.addEventListener('keypress', function(e) {
+    if(e.key === 'Enter'){
+        guardarNombre();
+    }
+});
+
+//comprobar si ya hay un nombre guardado
+//si hay un nombre guardado lo muestro
+const nombreGuardado = localStorage.getItem('nombreUsuario');
+if(nombreGuardado){
+    mostrarNombre(nombreGuardado);
+}
